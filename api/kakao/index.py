dca6e68d488187a31cb279ec4ca676991be4e60b
codesktop <codesktop@foxmail.com>
2021-10-23 13:38:25 +0800

    init commit

--- api/kakao/index.py
+++ api/kakao/index.py
@@ -5,10 +5,10 @@ from authlib.integrations.requests_client import OAuth2Session
 app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>', methods=['GET'])
-def weibo(path):
-  session = OAuth2Session(environ.get('WEIBO_CLIENT_ID'),
-                          environ.get('WEIBO_CLIENT_SECRET'),
-                          redirect_uri=environ.get('WEIBO_REDIRECT_URI'),
-                          scope=environ.get('WEIBO_SCOPE'))
-  authorization_url, state = session.create_authorization_url('https://api.weibo.com/oauth2/authorize')
+def kakao(path):
+  session = OAuth2Session(environ.get('KAKAO_CLIENT_ID'),
+                          environ.get('KAKAO_CLIENT_SECRET'),
+                          redirect_uri=environ.get('KAKAO_REDIRECT_URI'),
+                          scope=environ.get('KAKAO_SCOPE'))
+  authorization_url, state = session.create_authorization_url('https://kauth.kakao.com/oauth/authorize')
   return {'authorization_url': authorization_url}
--- api/kakao/token.py
+++ api/kakao/token.py
@@ -5,15 +5,15 @@ from authlib.integrations.requests_client import OAuth2Session
 app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>', methods=['POST'])
-def weibo(path):
-  session = OAuth2Session(environ.get('WEIBO_CLIENT_ID'),
-                          environ.get('WEIBO_CLIENT_SECRET'),
-                          redirect_uri=environ.get('WEIBO_REDIRECT_URI'),
-                          scope=environ.get('WEIBO_SCOPE'))
+def kakao(path):
+  session = OAuth2Session(environ.get('KAKAO_CLIENT_ID'),
+                          environ.get('KAKAO_CLIENT_SECRET'),
+                          redirect_uri=environ.get('KAKAO_REDIRECT_URI'),
+                          scope=environ.get('KAKAO_SCOPE'))
 
   try:
     authorization_response = request.form.get('authorization_response')
-    return session.fetch_token('https://api.weibo.com/oauth2/access_token',
+    return session.fetch_token('https://kauth.kakao.com/oauth/token',
                                grant_type='authorization_code',
                                authorization_response=authorization_response)
   except Exception as exception:
